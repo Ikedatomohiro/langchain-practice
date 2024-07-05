14cc559ffd6b5d01b5f987de58436a81128cfae8
Tomohiro Ikeda <to.tomo.tomohiro@gmail.com>
2024-07-05 22:31:32 +0900

    ai chat

---
+++ langchain-sample/chat_models.py
@@ -0,0 +1,14 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
+
+chat = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.5)
+
+messages = [
+    SystemMessage(content="You are a helpful assistant."),
+    HumanMessage(content="こんにちは！私はジョンといいます"),
+    AIMessage(content="こんにちはジョンさん！どのようにお手伝いできますか？"),
+    HumanMessage(content="私の名前がわかりますか？")
+]
+
+result = chat.invoke(messages)
+print(result.content)
