bf33e58c164fc57f7cc87a3465c367ab356b687c
Tomohiro Ikeda <to.tomo.tomohiro@gmail.com>
2024-07-06 23:02:04 +0900

    edit simple_chat

--- langchain-sample/simple_chat.py
+++ langchain-sample/simple_chat.py
@@ -1,5 +1,8 @@
+import os
+from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 
+load_dotenv()
 chat = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.5)
 result = chat.invoke("hello")
 
