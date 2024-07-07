1285a998f454babdabc1115d02e3fc193e56513c
Tomohiro Ikeda <to.tomo.tomohiro@gmail.com>
2024-07-07 21:41:06 +0900

    message history

---
+++ langchain-sample/tutrials/message-history.py
@@ -0,0 +1,57 @@
+import os
+from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+from langchain_core.chat_history import (
+    BaseChatMessageHistory,
+    InMemoryChatMessageHistory,
+)
+from langchain_core.runnables.history import RunnableWithMessageHistory
+
+load_dotenv()
+model = ChatOpenAI(model="gpt-3.5-turbo")
+
+store = {}
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = InMemoryChatMessageHistory()
+    return store[session_id]
+
+with_message_history = RunnableWithMessageHistory(model, get_session_history)
+
+config = {"configurable": {"session_id": "abc2"}}
+
+response = with_message_history.invoke(
+    [HumanMessage(content="Hi! I'm Bob")],
+    config=config,
+)
+
+print(response.content)
+
+response = with_message_history.invoke(
+    [HumanMessage(content="What's my name?")],
+    config=config,
+)
+
+print(response.content)
+# sessionが変わると履歴は別のものとして扱われる
+config = {"configurable": {"session_id": "abc3"}}
+
+response = with_message_history.invoke(
+    [HumanMessage(content="What's my name?")],
+    config=config,
+)
+
+print(response.content)
+
+# session_idが同じなら同じ履歴を参照する
+config = {"configurable": {"session_id": "abc2"}}
+
+response = with_message_history.invoke(
+    [HumanMessage(content="What's my name?")],
+    config=config,
+)
+
+print(response.content)
+
