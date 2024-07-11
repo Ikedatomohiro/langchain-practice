f6fedd4a68457a0acd1e77676d15327ebf69efc8
Tomohiro Ikeda <to.tomo.tomohiro@gmail.com>
2024-07-11 21:49:24 +0900

    add conversation

---
+++ langchain-sample/tutrials/conversational-rag/conversationlal.py
@@ -0,0 +1,108 @@
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+import bs4
+from langchain import hub
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_chroma import Chroma
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.chains import create_history_aware_retriever
+from langchain_core.prompts import MessagesPlaceholder
+from langchain_core.messages import AIMessage, HumanMessage
+
+load_dotenv()
+llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
+
+# 1. Load, chunk and index the contents of the blog to create a retriever.
+loader = WebBaseLoader(
+    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+    bs_kwargs=dict(
+        parse_only=bs4.SoupStrainer(
+            class_=("post-content", "post-title", "post-header")
+        )
+    ),
+)
+docs = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+splits = text_splitter.split_documents(docs)
+vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+retriever = vectorstore.as_retriever()
+
+# 2. Incorporate the retriever into a question-answering chain.
+system_prompt = (
+    "You are an assistant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer "
+    "the question. If you don't know the answer, say that you "
+    "don't know. Use three sentences maximum and keep the "
+    "answer concise."
+    "\n\n"
+    "{context}"
+)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        ("human", "{input}"),
+    ]
+)
+
+question_answer_chain = create_stuff_documents_chain(llm, prompt)
+rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+
+# ここまでは単純なドキュメントに対する質問
+response = rag_chain.invoke({"input": "What is Task Decomposition?"})
+print(response["answer"])
+# Task decomposition involves breaking down complex tasks into smaller and simpler steps. Techniques like Chain of Thought (CoT) and Tree of Thoughts help models decompose hard tasks into manageable sub-tasks by exploring multiple reasoning possibilities at each step. Task decomposition can be facilitated by simple prompting, task-specific instructions, or human inputs to assist in planning and executing tasks effectively.
+
+# 履歴を持って質問できるようにする
+contextualize_q_system_prompt = (
+    "Given a chat history and the latest user question "
+    "which might reference context in the chat history, "
+    "formulate a standalone question which can be understood "
+    "without the chat history. Do NOT answer the question, "
+    "just reformulate it if needed and otherwise return it as is."
+)
+
+contextualize_q_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", contextualize_q_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+history_aware_retriever = create_history_aware_retriever(
+    llm, retriever, contextualize_q_prompt
+)
+
+qa_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+
+question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+
+rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+
+chat_history = []
+
+question = "What is Task Decomposition?"
+ai_msg_1 = rag_chain.invoke({"input": question, "chat_history": chat_history})
+chat_history.extend(
+    [
+        HumanMessage(content=question),
+        AIMessage(content=ai_msg_1["answer"]),
+    ]
+)
+
+second_question = "What are common ways of doing it?"
+ai_msg_2 = rag_chain.invoke({"input": second_question, "chat_history": chat_history})
+
+print(ai_msg_2["answer"])
+# Task decomposition can be done in common ways such as using prompts with LLMs, providing task-specific instructions, or incorporating human inputs. For example, prompts like "Steps for XYZ" can guide the decomposition process, while task-specific instructions like "Write a story outline" can help in breaking down tasks. Human inputs can also play a role in guiding the decomposition of tasks into smaller steps.
